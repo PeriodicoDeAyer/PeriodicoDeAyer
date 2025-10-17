d410489d2c078db1b26a23ffc7b2f0ee0f004a33
Daniella Pacheco <daniellak.pacheco@gmail.com>
2025-10-17 13:16:54 +0200

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/femcoders/periodico_ayer/PeriodicoAyerApplication.java
@@ -0,0 +1,13 @@
+package com.femcoders.periodico_ayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeriodicoAyerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeriodicoAyerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/femcoders/periodico_ayer/PeriodicoAyerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.femcoders.periodico_ayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeriodicoAyerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
