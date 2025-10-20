403cccb647b36a040a907943d5d3eb9238cd42c9
AnaAguileraMorales88 <anitaaguilera@hotmail.com>
2025-10-20 12:18:01 +0200

    Feat/@Entity(implemented article.java)

--- src/main/java/com/femcoders/periodico_ayer/entity/Article.java
+++ src/main/java/com/femcoders/periodico_ayer/entity/Article.java
@@ -1,5 +1,37 @@
 package com.femcoders.periodico_ayer.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "articles")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String title;
+
+    @Column(nullable = false, length = 2000)
+    private String content;
+
+    private LocalDateTime publicationDate;
+
+    private String category;
     
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
