fa4d8e5ac7c383fa08712b0d869db0bd59d7c42b
Andrea <aoliverar.96@gmail.com>
2025-10-20 13:01:03 +0200

    Entity>User.java DONE

--- src/main/java/com/femcoders/periodico_ayer/entity/User.java
+++ src/main/java/com/femcoders/periodico_ayer/entity/User.java
@@ -1,5 +1,27 @@
 package com.femcoders.periodico_ayer.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name="users")
+@Data
 public class User {
     
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Article> articles = new ArrayList<>();
+
 }
