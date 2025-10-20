d7af61d787e7dc12d86a27e13a8e486e786ba01e
AnaAguileraMorales88 <anitaaguilera@hotmail.com>
2025-10-20 14:49:23 +0200

    feat/repository(implemend use repository)

--- src/main/java/com/femcoders/periodico_ayer/repository/UserRepository.java
+++ src/main/java/com/femcoders/periodico_ayer/repository/UserRepository.java
@@ -1,5 +1,21 @@
 package com.femcoders.periodico_ayer.repository;
 
-public class UserRepository {
+
+import com.femcoders.periodico_ayer.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
 
 }
