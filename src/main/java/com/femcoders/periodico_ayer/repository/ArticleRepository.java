fa0c08b5b6a4d9d7db10f47b7f9428a0c53b7791
Daniella Pacheco <daniellak.pacheco@gmail.com>
2025-10-20 13:25:14 +0200

    feat/repository(implemated article repository)

--- src/main/java/com/femcoders/periodico_ayer/repository/ArticleRepository.java
+++ src/main/java/com/femcoders/periodico_ayer/repository/ArticleRepository.java
@@ -1,5 +1,15 @@
 package com.femcoders.periodico_ayer.repository;
 
-public class ArticleRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.femcoders.periodico_ayer.entity.Article;
+import java.util.List;
+import com.femcoders.periodico_ayer.entity.User;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+    List<Article> findByTitleContainingIgnoreCase(String title);
+
+    List<Article> findByCategory(String category);
+
+    List<Article> findByUser(User user);
+
 }
